c0bae66a5f8014ed46be339cce82161417de1a16
han <dy__z@nate.com>
2022-08-23 20:16:24 +0900

    make chat list

--- src/components/chatboard/chatboard.jsx
+++ src/components/chatboard/chatboard.jsx
@@ -3,8 +3,69 @@ import './chatboard.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faPaperPlane} from '@fortawesome/free-solid-svg-icons';
 import {faFaceSmile} from '@fortawesome/free-solid-svg-icons';
+import img from './jordan.JPG'
 const Chatboard = (props) => (
-        <div className="b_container">
+    <>
+    <div className="b_wrap">
+        <div className="b_left">
+            <div className="chatList">
+                    <ul className="chatRows">
+                        <li className="chatList">
+                            <div className="li_img">
+                                <img src={img} alt="" />
+                            </div>
+                            <div className="li_description">
+                                <div className="li_left">
+                                    <p>Jordan1</p>
+                                    <p>I like it!</p>
+                                </div>
+                                <div className="li_right">
+                                    <p>09:00</p>
+                                    <div className="msgCount">
+                                    <p>3</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="chatList">
+                            <div className="li_img">
+                                <img src={img} alt="" />
+                            </div>
+                            <div className="li_description">
+                                <div className="li_left">
+                                    <p>Jordan1</p>
+                                    <p>I like it!</p>
+                                </div>
+                                <div className="li_right">
+                                    <p>09:00</p>
+                                    <div className="msgCount">
+                                    <p>3</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="chatList">
+                            <div className="li_img">
+                                <img src={img} alt="" />
+                            </div>
+                            <div className="li_description">
+                                <div className="li_left">
+                                    <p>Jordan1</p>
+                                    <p>I like it!</p>
+                                </div>
+                                <div className="li_right">
+                                    <p>09:00</p>
+                                    <div className="msgCount">
+                                    <p>3</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+            </div>
+        </div>
+
+        <div className="b_right">
             <div className="b_board">
                 <ul className='messages'>
                     <li className="message other">Hi there, How are you?</li>
@@ -42,6 +103,9 @@ const Chatboard = (props) => (
                 </div>
             </div>
         </div>
+    </div>
+    </>
+        
     );
 
 export default Chatboard;
